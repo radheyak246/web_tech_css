b7a661bb8fd050dbd9ee75fc26a9464757b1fdbd
radheyak246 <radheyak246@gmail.com>
2026-09-11 08:56:35 +0530

    UPLOAD MORE FACULTY

--- WT_ASSIGNMENT-3/script.js
+++ WT_ASSIGNMENT-3/script.js
@@ -5,93 +5,474 @@
 const faculty = [
 
     {
-        name: "Mr. Tamal Pal",
+        name: "Dr. Abhik Mukherjee",
 
-        photo: "tamal_pal.jpg",
+        photo: "abhikmukherjee.jpg",
 
-        email: " tamal@cs.iiests.ac.in",
+        email: "abhik@cs.iiests.ac.in",
 
         qualification: [
-            "Ph.D (Engineering), Computer Science and Technology, IIEST ",
-            "ME. Computer Science and Technology, IIEST",
-            "BTech. Computer Science and Engineering, MAKAUT"
+            "Ph.D. from Computer Science Department of BESU, Shibpur, 2003",
+            "Masters with Control Systems specialization, Jadavpur University, 1994",
+            "Bachelors in Electronics & Telecommunication Engineering, Jadavpur University, 1991"
         ],
 
         subjects: [
-           "Computer Graphics",
-           "Introduction to Computing",
-           "Programming Paradigm",
-           "Web Technology",
-           "Software Engineering",
-           "Peripheral Devices and Interfaces"
+            "Signals and Systems",
+"Design and Analysis of Algorithm",
+"Simulation and Modeling Lab"
         ],
 
         research: [
-            "Computer Vision: Multimedia (Image, Video, etc.) processing ",
-            "Multimedia communication over Wireless Networks (WMSN, DTN, VANET, etc.)",
-            "Multimedia security",
-            "AIML(Artificial Intelligence & Machine Learning)",
-            "Games programming"
+            "Estimation theory for Meteorology",
+"Estimation Theory for Navigation and Control Applications",
+"Information systems and related algorithm",
+"Estimation and Control applied to Systems Biology",
+"Online social media analysis",
+"Control applications for energy systems"
         ]
     },
 
 
     {
-        name: "Mr. Surajeet Ghosh",
+        name: "Dr. Amit Biswas",
 
-        photo: "surajeet.png",
+        photo: "images/amitbiswas.png",
 
-        email: "surajeet@cs.iiests.ac.in",
+        email: "amitbiswas@cs.iiests.ac.in",
 
-        qualification: ["Ph.D. (Engineering), Jadavpur University, Kolkata, India",
-            "M.E. (Computer Science and Engineering), Maulana Abul Kalam Azad University of Technology, (Formerly West Bengal University of Technology), Kolkata, India",
-            "B.Tech. (Computer Science and Technology), University of Kalyani, Kalyani, West Bengal, India"
+        qualification: ["B.Tech in Computer Science and Engineering, Jalpaiguri Government Engineering College, West Bengal",
+"M.Tech in Computer Science and Engineering, NIT Durgapur, Durgapur, West Bengal",
+"Ph.D. in Computer Science and Engineering, IIT(BHU), Varanasi, Uttar Pradesh"],
+
+        subjects: ["Design and Analysis of Algorithm",
+"Programming Paradigms",
+"Operating Systems",
+"Data Structure",
+"Introduction to AI and ML"],
+
+        research: [
+            "Distributed Systems",
+            "Cloud Computing",
+            "Multi-criteria Decision Making (MCDM)",
+            "Blockchain Technology",
+            "Quantum Computing"
+        ]
+    },
+
+
+    {
+        name: "Dr. Apurba Sarkar",
+
+        photo: "image/apurba.jpg",
+
+        email: "apurba@cs.iiests.ac.in",
+
+        qualification: [
+            "B.Tech. (Computer Science and Engineering), University of Calcutta, 2001",
+            "M.Tech. (Computer Science and Engineering), IIT Guwahati, 2003",
+            "Ph.D. (Computer Science and Engineering), IIT Kanpur, 2016"
+        ],
+
+        subjects: [
+            "Data Structure and Algorithm",
+            "Discrete Structure",
+            "Theory of Computation",
+            "Operating System",
+            "Object Oriented Programming",
+            "Topics in Operating System (Advanced)"
+        ],
+
+        research: [
+            "Digital Geometry",
+            "Combinatorial Geometry",
+            "Discrete Geometry",
+            "Computational Geometry",
+            "Image Processing",
+            "Shape Analysis",
+            "Shape Classification",
+            "Text Summarization"
+        ]
+    },
+
+
+    {
+        name: "Mr. Ashish Kumar Layek",
+
+        photo: "images/ashishlayek_cst.jpg",
+
+        email: "ashish@cs.iiests.ac.in",
+
+        qualification: ["M.E. in Computer Science and Technology, Bengal Engineering College (DU), 2001",
+"B.E. in Computer Science and Technology, Bengal Engineering College (DU), 1999"],
+
+        subjects: [
+            "Programming Paradigms",
+            "Mobile Computing",
+            "Computer Network",
+            "Software Engineering",
+            "Database Management System"
+        ],
+
+        research: [
+            "Wireless Telecommunication and Networking",
+            "Pattern Recognition",
+            "Computer Vision and Digital Image Processing"
+        ]
+    },
+
+
+    {
+        name: "Dr. Asit Kumar Das",
+
+        photo: "images/asitkumar.png",
+
+        email: "akdas@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D., Bengal Engineering and Science University, 2011",
+            "M.Tech. in Computer Science & Engineering, Calcutta University, 2002",
+            "B.Tech. in Computer Science & Engineering, Calcutta University, 1996",
+            "B.Sc. Honours in Mathematics, Calcutta University, 1993",
+            "Higher Secondary, West Bengal Council of Higher Secondary Education, 1990",
+            "Madhyamik, West Bengal Board of Secondary Education, 1988"
         ],
 
         subjects: [
             "Introduction to Computing",
-            "Peripheral Devices and Interfaces",
-            "Computer Networks",
-            "Embedded Systems",
-            "Reconfigurable Computing",
-            "Network Security"
+            "Data Structures and Algorithms"
         ],
 
         research: [
-            "Machine Learning Based Optimization for IoT Protocol",
-            "Hardware Accelerators for Machine Learning Applications",
-            "IPv6 Routing Protocol for Low-Power and Lossy Networks (RPL)",
-            "Machine Learning for Embedded Systems",
-            "Routing Protocol for IoT Networks",
-            "FPGA Based Embedded Systems Design",
-            "Hardware Architecture for Network Routing Schemes",
-            "Computational Architecture for Next Generation Sequencing"
+            "Data Mining and Pattern Recognition",
+            "Social Networks",
+            "Bioinformatics",
+            "Machine Learning and Soft Computing",
+            "Text, Audio and Video Data Analysis"
         ]
     },
 
 
     {
-        name: "Mrs. Sipra Das Bit",
+        name: "Dr. Biplab Kumar Sikdar",
+
+        photo: "images/cst_bksikdar.jpg",
+
+        email: "biplab@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D., Bengal Engineering and Science University, Shibpur, 2003",
+            "M.Tech. in Computer Science & Engineering, Calcutta University, 1990",
+            "B.Tech. in Computer Science & Engineering, Calcutta University, 1988",
+            "B.Sc. Honours in Physics, Presidency College, Calcutta University, 1985"
+        ],
+
+        subjects: ["Computer Architecture and organisation -2"],
+
+        research: [
+            "Cellular Automata",
+            "Computer Architecture"
+        ]
+    },
+
+
+    {
+        name: "Dr. Jaya Sil",
+
+        photo: "images/jayasil.jpeg",
+
+        email: "js@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D. (Computer Science), specialization in Artificial Intelligence and Soft Computing, 1996",
+            "M.E. (Computer Science & Engineering), 1st Class, 1986",
+            "B.E. (Electronics & Telecommunication), 1st Class, 1984"
+        ],
+
+        subjects: ["Soft Computing",
+"Artificial Intelligence",
+"Machine Learning"],
+
+        research: [
+            "Image Processing and Computer Vision",
+            "Machine Learning",
+            "Bio-Informatics"
+        ]
+    },
+
+
+    {
+        name: "Dr. Malay Kule",
+
+        photo: "images/malay.jp",
+
+        email: "malay@cs.iiests.ac.in",
+
+        qualification: [
+            "M.Tech., Computer Science and Engineering, University of Calcutta",
+            "B.Tech., Computer Science and Engineering, University of Calcutta",
+            "B.Sc., Physics Honours, University of Calcutta"
+        ],
+
+        subjects: [
+            "Introduction to Computing",
+            "Operating System",
+            "Database Management System",
+            "Information Security and Cryptography",
+            "Introduction to Soft Computing"
+        ],
+
+        research: [
+            "Defect Tolerance of Nanoscale Crossbar Circuits",
+            "Cryptology"
+        ]
+    },
+
+
+    {
+        name: "Mr. Manas Hira",
+
+        photo: "images/cst_mhira",
+
+        email: "manas@cs.iiests.ac.in",
+
+        qualification: [
+            "M.Tech., IIT Kharagpur, 1991",
+            "B.Tech., IIT Kharagpur, 1989"
+        ],
+
+        subjects: [
+            "Theory of Computation",
+            "Operating Systems",
+            "Computer Architecture",
+            "Principles of Programming Language",
+            "Introduction to Computing",
+            "Microprocessor Laboratory",
+            "Programming Paradigms Laboratory",
+            "Computer Architecture and Organization Laboratory",
+            "Operating System Laboratory",
+            "Network Laboratory",
+            "Computing Practice Laboratory"
+        ],
+
+        research: [
+            "Circuit Verification using Temporal Logic"
+        ]
+    },
+
+
+    {
+        name: "Dr. Nirnay Ghosh",
+
+        photo: "images/nirnay.jpg",
+
+        email: "nirnay@cs.iiests.ac.in",
+
+        qualification: [
+            "Doctor of Philosophy (PhD), IIT Kharagpur, 2016",
+            "Master of Science (MS by Research), IIT Kharagpur, 2010",
+            "Bachelor of Technology (BTech - Computer Science and Engineering), West Bengal University of Technology, 2006"
+        ],
+
+        subjects: ["Discrete Structures",
+"Computer Networks"],
+
+        research: [
+            "Federated Learning",
+            "Blockchain and its applications",
+            "Internet of Things (IoT) - Applications, Security, and System Integration",
+            "Information/Network Security",
+            "Mobile Crowdsensing",
+            "Cloud Computing"
+        ]
+    },
+
+
+    {
+        name: "Dr. Samit Biswas",
+
+        photo: "images/samit.jpg",
+
+        email: "samit@cs.iiests.ac.in",
+
+        qualification: [
+            "B.E. in Computer Science and Engineering, University of Burdwan",
+            "M.Tech. in Computer Science and Technology, Kalyani University",
+            "Ph.D. in Computer Science and Technology, IIEST Shibpur"
+        ],
+
+        subjects: [
+            "Introduction To Computing",
+            "Compiler Design",
+            "Formal Language and Automata Theory"
+        ],
+
+        research: [
+            "Image Processing and Pattern Recognition",
+            "Document Image Analysis",
+            "Machine Learning",
+            "Machine-based Translation",
+            "Digital Image Processing and Analysis"
+        ]
+    },
+
+
+  
+
+    {
+        name: "Dr. Sekhar Mandal",
+
+        photo: "images/sekhar__cst.jpg",
+
+        email: "sekhar@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D., Department of Computer Science and Technology, Bengal Engineering and Science University, Shibpur, 2007",
+            "M.Tech., Department of Radio Physics and Electronics, University of Calcutta, 1993",
+            "B.Tech., Department of Radio Physics and Electronics, University of Calcutta, 1991"
+        ],
+
+        subjects: ["Introducting to Computing",
+"Data Mining",
+"Digital Image Processing",
+"Database Management Systems",
+"Digital Logic"],
+
+        research: [
+            "Computer Vision",
+            "Pattern Recognition",
+            "Image Processing"
+        ]
+    },
+
+
+    {
+        name: "Dr. Sipra Das Bit",
 
         photo: "sipradasbit.jpg",
 
         email: "sb@cs.iiests.ac.in",
 
-        qualification: [ "Ph.D(Engg) from Dept. of Computer Sc. & Engg.from Jadavpur University, Kolkata, 1997"
+        qualification: [
+            "Ph.D. (Engineering), Department of Computer Science & Engineering, Jadavpur University, Kolkata, 1997"
         ],
-        
-        subjects: [  
-            "Database Management System",
-            "Advanced Database Management System"
-        ],
-          
+
+        subjects: [" Advanced Database Management System"],
 
         research: [
-            " IoT and its usage in Industrial Informatics",
-            "Delay Tolerant Network and its usage in Smart City and Post-disaster management system",
+            "Mobile Computing",
             "Wireless Sensor Network",
+            "Delay Tolerant Network",
+            "IoT and its usage in Industrial Informatics",
+            "Delay Tolerant Network and its usage in Smart City and Post-disaster management system"
+        ]
+    },
+
+
+    {
+        name: "Dr. Sulata Mitra",
+
+        photo: "images/sulata mitra.jpg",
+
+        email: "sulata@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D."
+        ],
+
+        subjects: [
+            "Operating System",
+            "Computer Network",
+            "Digital Logic",
+            "Computer Organization and Architecture",
+            "Electronic Design and Automation"
+        ],
+
+        research: [
+            "Ad-hoc Network",
+            "Multihomed Mobile Network",
             "Mobile Computing"
         ]
+    },
+
+
+    {
+        name: "Dr. Surajeet Ghosh",
+
+        photo: "surajeet.png",
+
+        email: "surajeet@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D. (Engineering), Jadavpur University, Kolkata, India",
+            "M.E. (Computer Science and Engineering), Maulana Abul Kalam Azad University of Technology, Kolkata, India",
+            "B.Tech. (Computer Science and Technology), University of Kalyani, West Bengal, India"
+        ],
+
+        subjects: [
+            "Reconfigurable Computing",
+            "Embedded Systems",
+            "Computer Organization and Architecture",
+            "Computer Networks",
+            "Peripheral Devices and Interfaces",
+            "Introduction to Computing"
+        ],
+
+        research: [
+            "Custom Computing",
+            "Computational Architecture for Next Generation Sequencing",
+            "Hardware Architecture for Network Routing Schemes"
+        ]
+    },
+
+
+   
+
+    {
+        name: "Dr. Tamal Pal",
+
+        photo: "tamal_pal.jpg",
+
+        email: "tamal@cs.iiests.ac.in",
+
+        qualification: [
+            "Ph.D (Engineering), Computer Science and Technology, IIEST",
+"ME. Computer Science and Technology, IIEST",
+"BTech. Computer Science and Engineering, MAKAUT"
+        ],
+
+        subjects: ["Computer Graphics",
+"Introduction to Computing",
+"Programming Paradigm",
+"Web Technology",
+"Software Engineering",
+"Peripheral Devices and Interfaces"],
+
+        research: [
+            "Image Processing",
+            "Wireless Multi-media Sensor Network"
+        ]
+    },
+
+
+
+   
+
+    {
+        name: "Dr. Soumen Moulik",
+
+        photo: "images/soumen moulik.jpg",
+
+        email: "soumen@cs.iiests.ac.in",
+
+        qualification: ["PhD from Indian Institute of Technology Kharagpur"],
+
+        subjects: ["Machine learning"],
+
+        research: ["Analysis of sensor-acquired raw data using Machine Learning algorithms",
+"MAC layer functionalities of IEEE 802.15.4 and IEEE 802.15.6 standard",
+"QoS provisioning in Wireless Personal / Body Area Networks (WPANs/WBANs)",
+"Real-time and ubiquitous IoT system development (mainly for healthcare applications)"]
     }
 
 ];
